a2f7757b69d9be298187d56ef1703eb7355035eb
Ttrần Pphú <38020975+TtranPphu@users.noreply.github.com>
2025-04-14 07:45:45 +0000

    Rename init.sh -> initialize.sh

--- start-nvim-container-little-example.sh
+++ start-nvim-container-little-example.sh
@@ -1,2 +1,2 @@
-sh init.sh && docker compose up -d little-example-devcontainer --build && docker exec -it --workdir /workspaces/little-startup little-startup-little-example-devcontainer-1 nvim
+sh initialize.sh && docker compose up -d little-example-devcontainer --build && docker exec -it --workdir /workspaces/little-startup little-startup-little-example-devcontainer-1 nvim
 
