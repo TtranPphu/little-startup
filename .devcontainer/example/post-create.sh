e469a955f73f8336ad4c51dbd2b70910e8d11872
TtranPphu <ttranpphu0212@gmail.com>
2025-04-15 02:43:31 +0000

    Stash

--- .devcontainer/example/post-create.sh
+++ .devcontainer/example/post-create.sh
@@ -3,4 +3,4 @@
 cp .devcontainer/pre-commit .git/hooks/pre-commit
 git config core.editor "nvim"
 
-(cd little-example; uv sync; npm install)
+(cd example;. $HOME/.local/bin/env && uv sync; npm install)
--- nvim-dev.sh
+++ nvim-dev.sh
@@ -1,16 +1,15 @@
 #! /usr/bin/bash
-
+sh initialize.sh
 case $1 in
-  example)
-    sh initialize.sh && docker compose up -d example-devcontainer --build && docker exec -it --workdir /workspaces/little-startup little-startup-example-devcontainer-1 nvim
+  example | ex)
+    docker compose up -d example-devcontainer --build --remove-orphans && docker exec -it --workdir /workspaces/little-startup little-startup-example-devcontainer-1 sh .devcontainer/example/post-create.sh; nvim
     ;;
-  frontend)
-    sh initialize.sh && docker compose up -d frontend-devcontainer --build && docker exec -it --workdir /workspaces/little-startup little-startup-frontend-devcontainer-1 nvim
+  frontend | FE | fe)
+    docker compose up -d frontend-devcontainer --build --remove-orphans && docker exec -it --workdir /workspaces/little-startup little-startup-frontend-devcontainer-1 sh .devcontainer/frontend/post-create.sh; nvim
     ;;
-  backend)
-    sh initialize.sh && docker compose up -d backend-devcontainer --build && docker exec -it --workdir /workspaces/little-startup little-startup-backend-devcontainer-1 nvim
+  backend | BE | be)
+    docker compose up -d backend-devcontainer --build --remove-orphans && docker exec -it --workdir /workspaces/little-startup little-startup-backend-devcontainer-1 sh .devcontainer/backend/post-create.sh; nvim
     ;;
-
 esac
 docker compose stop
 
