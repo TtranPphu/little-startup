080b477214ce7f1733b03118e2f0f23482991b83
TtranPphu <ttranpphu0212@gmail.com>
2025-04-16 14:30:59 +0700

    Stash @ work

--- nvim-dev.sh
+++ nvim-dev.sh
@@ -4,43 +4,29 @@ sh initialize.sh
 
 case $1 in
   example | ex | eg)
-    docker compose up -d example-devcontainer --build --remove-orphans && \
-    docker exec \
-      --workdir /workspaces/little-startup \
-      little-startup-example-devcontainer-1 \
-      sh .devcontainer/example/post-create.sh && \
-    docker exec -it \
-      --workdir /workspaces/little-startup \
-      little-startup-example-devcontainer-1 \
-      nvim
+    SERVICE='example'
     ;;
-  frontend | FE | fe)
-    docker compose up -d frontend-devcontainer --build --remove-orphans && \
-    docker exec \
-      --workdir /workspaces/little-startup \
-      little-startup-frontend-devcontainer-1 \
-      sh .devcontainer/frontend/post-create.sh && \
-    docker exec -it \
-      --workdir /workspaces/little-startup \
-      little-startup-frontend-devcontainer-1 \
-      nvim
+ frontend | FE | fe)
+    SERVICE='frontend'
     ;;
   backend | BE | be)
-    docker compose up -d backend-devcontainer --build --remove-orphans && \
-    docker exec \
-      --workdir /workspaces/little-startup \
-      little-startup-backend-devcontainer-1 \
-      sh .devcontainer/backend/post-create.sh && \
-    docker exec -it \
-      --workdir /workspaces/little-startup \
-      little-startup-backend-devcontainer-1 \
-      nvim
+    SERVICE='backend'
     ;;
   *)
     printf "Missing <service>, try:\n  - sh nvim-dev.sh example\n  - sh nvim-dev.sh backend\n  - sh nvim-dev.sh frontend\n"
     exit
     ;;
 esac
+
+docker compose up -d $SERVICE-devcontainer --build --remove-orphans && \
+docker exec \
+  --workdir /workspaces/little-startup \
+  little-startup-$SERVICE-devcontainer-1 \
+  sh .devcontainer/$SERVICE/post-create.sh && \
+docker exec -it \
+  --workdir /workspaces/little-startup \
+  little-startup-$SERVICE-devcontainer-1 \
+  nvim
 
 docker compose stop
 
