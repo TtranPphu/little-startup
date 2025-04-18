328372059d696593328b29093cc1c352aa2b0214
TtranPphu <ttranpphu0212@gmail.com>
2025-04-18 18:28:42 +0700

    Sync host's nvim with remote

--- initialize.sh
+++ initialize.sh
@@ -4,3 +4,5 @@ sed -i "s/<host-username>/$USER/g" docker-compose.yml
 
 # dummy nvim config
 mkdir ~/.config/nvim 2>/dev/null | true
+mkdir ~/.local/share/nvim 2>/dev/null | true
+mkdir ~/.local/state/nvim 2>/dev/null | true
