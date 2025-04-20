4cdb7461433ed5f4ee62bcc18c90320438a4520d
TtranPphu <ttranpphu0212@gmail.com>
2025-04-20 06:58:28 +0000

    Fixing dev con home folder's files permission, improve zsh initiation time

--- initialize.sh
+++ initialize.sh
@@ -6,3 +6,4 @@ sed -i "s/<host-username>/$USER/g" docker-compose.yml
 mkdir ~/.config/nvim 2>/dev/null | true
 mkdir ~/.local/share/nvim 2>/dev/null | true
 mkdir ~/.local/state/nvim 2>/dev/null | true
+
