9c893f9c5b7a65bc7fe1173787ce25f9091a3e7d
Jay Dao <duytung13pro@gmail.com>
2025-04-27 13:58:31 +0700

    Add --legact to post-create

--- .devcontainer/frontend/post-create.sh
+++ .devcontainer/frontend/post-create.sh
@@ -5,7 +5,7 @@ sed -i "s/$HOST_USERNAME/<host-username>/g" docker-compose.yml
 cp .devcontainer/pre-commit .git/hooks/pre-commit
 git config core.editor "nvim"
 
-(cd frontend/student-fe; npm install --force)
-(cd frontend/tutor-fe; npm install --force)
-(cd frontend/faculty-fe; npm install --force)
+(cd frontend/student-fe; npm install --legacy-peer-deps)
+(cd frontend/tutor-fe; npm install --legacy-peer-deps)
+(cd frontend/faculty-fe; npm install --legacy-peer-deps)
 
