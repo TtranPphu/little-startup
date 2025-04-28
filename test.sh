16919bc374c9e8d4fb43d011cb129d846ac96efa
TtranPphu <ttranpphu0212@gmail.com>
2025-04-28 18:18:36 +0700

    Stash @ work

---
+++ test.sh
@@ -0,0 +1,21 @@
+sudo git clean -f -d -x -e ".db-*"
+sh initialize.sh
+printf 'Testing...\n'
+docker compose build --parallel $SERVICES && \
+docker compose up -d --remove-orphans $SERVICES && \
+for context in 'example' 'backend' 'frontend'; do
+  service="$context-devcontainer"
+  container="little-startup-$service-1"
+  printf "Initiating $service...\n"
+  docker exec \
+    --workdir /workspaces/little-startup \
+    $container \
+    sh -c ".devcontainer/$context/post-create.sh; exit \$?"
+done && \
+docker compose down
+if [ $? != 0 ]; then
+  printf "Testing... Failed. You suck!\n"
+else
+  printf "Testing... Done. You're awesome!\n"
+fi
+sed -i "s/$USER/<host-username>/g" docker-compose.yml
