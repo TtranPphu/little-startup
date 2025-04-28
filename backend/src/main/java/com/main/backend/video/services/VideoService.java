fc6baca8fe7e042ba3e085e393e4bf9cdb4d7e2e
phuccuhp211 <phuccuhp211@gmail.com>
2025-04-28 17:43:09 +0000

    add try-catch block

--- backend/src/main/java/com/main/backend/video/services/VideoService.java
+++ backend/src/main/java/com/main/backend/video/services/VideoService.java
@@ -1,15 +1,16 @@
 package com.main.backend.video.services;
 
+import com.main.backend.common.exceptions.BusinessException;
 import com.main.backend.video.dtos.VideoAddResponseDto;
 import com.main.backend.video.dtos.VideoDeleteResponseDto;
 import com.main.backend.video.models.Video;
 import com.main.backend.video.repositories.VideoRepo;
 import com.main.backend.video.utils.FFmpegConverter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
-import java.io.IOException;
 import java.util.Optional;
 
 @Service
@@ -18,78 +19,80 @@ public class VideoService {
 
     private final VideoRepo videoRepo;
 
-    public VideoAddResponseDto addVideo(MultipartFile file) throws IOException, InterruptedException {
-        if (file.isEmpty()) {
-            throw new IllegalArgumentException("Uploaded file is empty!");
-        }
-
-        String basePath = System.getProperty("user.dir") + "/data/videos";
-        String originalFilename = file.getOriginalFilename();
-
-        if (originalFilename == null) {
-            throw new IllegalArgumentException("file's name can't not be empty!");
-        }
-
-        String filenameWithoutExt = originalFilename.replaceFirst("[.][^.]+$", "");
-        String sanitizedFilename = filenameWithoutExt.trim().replaceAll("\\s+", "-");
-        String uploadId = sanitizedFilename;
-
-        File uploadDir = new File(basePath, uploadId);
-        if (!uploadDir.exists()) {
-            uploadDir.mkdirs();
-        }
-
-        File uploadedFile = new File(uploadDir, file.getOriginalFilename());
-        file.transferTo(uploadedFile);
-
-        File hlsOutputDir = new File(uploadDir, "hls");
-        if (!hlsOutputDir.exists()) {
-            hlsOutputDir.mkdirs();
-        }
-        FFmpegConverter.convertMp4ToHls(uploadedFile, hlsOutputDir);
-
-        if (uploadedFile.exists()) {
-            boolean deleted = uploadedFile.delete();
-            if (!deleted) {
-                throw new IllegalArgumentException("Cant not delete origin file: " + uploadedFile.getAbsolutePath());
+    public VideoAddResponseDto addVideo(MultipartFile file) throws Exception {
+        try {
+            if (file.isEmpty()) {
+                throw new Exception("Uploaded file is empty!");
             }
+
+            String basePath = System.getProperty("user.dir") + "/data/videos";
+            String originalFilename = file.getOriginalFilename();
+
+            if (originalFilename == null) {
+                throw new Exception("file's name can't not be empty!");
+            }
+
+            String filenameWithoutExt = originalFilename.replaceFirst("[.][^.]+$", "");
+            String sanitizedFilename = filenameWithoutExt.trim().replaceAll("\\s+", "-");
+            String uploadId = sanitizedFilename;
+
+            File uploadDir = new File(basePath, uploadId);
+            if (!uploadDir.exists()) {
+                uploadDir.mkdirs();
+            }
+
+            File uploadedFile = new File(uploadDir, file.getOriginalFilename());
+            file.transferTo(uploadedFile);
+
+            File hlsOutputDir = new File(uploadDir, "hls");
+            if (!hlsOutputDir.exists()) {
+                hlsOutputDir.mkdirs();
+            }
+            FFmpegConverter.convertMp4ToHls(uploadedFile, hlsOutputDir);
+
+            if (uploadedFile.exists()) {
+                boolean deleted = uploadedFile.delete();
+                if (!deleted) {
+                    throw new Exception("Cant not delete origin file: " + uploadedFile.getAbsolutePath());
+                }
+            }
+
+            String m3u8Url = "/data/videos/" + uploadId + "/hls/index.m3u8";
+
+            Video video = Video.builder()
+                    .originalFilename(file.getOriginalFilename())
+                    .m3u8Url(m3u8Url)
+                    .storagePath(uploadDir.getAbsolutePath())
+                    .uploadTime(System.currentTimeMillis())
+                    .build();
+
+            Video saved = videoRepo.save(video);
+
+            return VideoAddResponseDto.builder().id(saved.getId()).m3u8Url(saved.getM3u8Url()).build();
+        } catch (Exception e) {
+            throw new BusinessException(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
-
-        String m3u8Url = "/data/videos/" + uploadId + "/hls/index.m3u8";
-
-        Video video = Video.builder()
-                .originalFilename(file.getOriginalFilename())
-                .m3u8Url(m3u8Url)
-                .storagePath(uploadDir.getAbsolutePath())
-                .uploadTime(System.currentTimeMillis())
-                .build();
-
-        Video saved = videoRepo.save(video);
-
-        return VideoAddResponseDto.builder()
-                .id(saved.getId())
-                .m3u8Url(saved.getM3u8Url())
-                .build();
     }
 
-    public VideoDeleteResponseDto deleteVideoById(String id) throws IOException, InterruptedException {
-        Optional<Video> optionalVideo = videoRepo.findById(id);
-        if (optionalVideo.isEmpty()) {
-            throw new IllegalArgumentException("Not found Video with ID: " + id);
+    public VideoDeleteResponseDto deleteVideoById(String id) throws Exception {
+        try {
+            Optional<Video> optionalVideo = videoRepo.findById(id);
+            if (optionalVideo.isEmpty()) {
+                throw new Exception("Not found Video with ID: " + id);
+            }
+
+            Video video = optionalVideo.get();
+
+            File storageDir = new File(video.getStoragePath());
+            if (storageDir.exists()) {
+                deleteFolderRecursively(storageDir);
+            }
+            videoRepo.delete(video);
+
+            return VideoDeleteResponseDto.builder().id(id).message("Video has deleted successfully!").build();
+        } catch (Exception e) {
+            throw new BusinessException(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
-
-        Video video = optionalVideo.get();
-
-        File storageDir = new File(video.getStoragePath());
-        if (storageDir.exists()) {
-            deleteFolderRecursively(storageDir);
-        }
-        videoRepo.delete(video);
-
-        return VideoDeleteResponseDto.builder()
-                .id(id)
-                .message("Video has deleted successfully!")
-                .build();
     }
 
     private void deleteFolderRecursively(File folder) {
--- backend/src/main/java/com/main/backend/video/utils/FFmpegConverter.java
+++ backend/src/main/java/com/main/backend/video/utils/FFmpegConverter.java
@@ -1,30 +1,34 @@
 package com.main.backend.video.utils;
 
 import java.io.File;
-import java.io.IOException;
 
 public class FFmpegConverter {
 
-    public static void convertMp4ToHls(File mp4File, File outputDir) throws IOException, InterruptedException {
-        if (!outputDir.exists()) {
-            outputDir.mkdirs();
+    public static void convertMp4ToHls(File mp4File, File outputDir) throws Exception {
+        try {
+            if (!outputDir.exists()) {
+                outputDir.mkdirs();
+            }
+
+            String outputPath = new File(outputDir, "index.m3u8").getAbsolutePath();
+            ProcessBuilder builder = new ProcessBuilder(
+                    "ffmpeg", "-i", mp4File.getAbsolutePath(),
+                    "-codec:", "copy",
+                    "-start_number", "0",
+                    "-hls_time", "5",
+                    "-hls_list_size", "0",
+                    "-f", "hls",
+                    outputPath);
+
+            builder.redirectErrorStream(true);
+            Process process = builder.start();
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new Exception("FFmpeg failed with exit code " + exitCode);
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
         }
 
-        String outputPath = new File(outputDir, "index.m3u8").getAbsolutePath();
-        ProcessBuilder builder = new ProcessBuilder(
-                "ffmpeg", "-i", mp4File.getAbsolutePath(),
-                "-codec:", "copy",
-                "-start_number", "0",
-                "-hls_time", "5",
-                "-hls_list_size", "0",
-                "-f", "hls",
-                outputPath);
-
-        builder.redirectErrorStream(true);
-        Process process = builder.start();
-        int exitCode = process.waitFor();
-        if (exitCode != 0) {
-            throw new RuntimeException("FFmpeg failed with exit code " + exitCode);
-        }
     }
 }
