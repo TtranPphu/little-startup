dc9c81941be548cfdccd29a1ef313efc785869dc
Jay Dao <duytung13pro@gmail.com>
2025-04-28 08:38:00 +0000

    Init student and faculty

--- frontend/tutor-fe/components/dashboard-layout.tsx
+++ frontend/tutor-fe/components/dashboard-layout.tsx
@@ -12,11 +12,11 @@ export function DashboardLayout({ children }: { children: React.ReactNode }) {
 
   return (
     <SidebarProvider defaultOpen={true}>
-      <div className="max-w-7xl mx-auto px-4">
+      <div className="flex min-h-screen flex-col">
         <Header />
         <div className="flex flex-1">
           <Sidebar />
-          <main className="flex-1 overflow-y-auto bg-background p-4 md:p-6">
+          <main className="flex-1 overflow-y-auto bg-background p-4 md:p-6 ">
             {children}
           </main>
         </div>
