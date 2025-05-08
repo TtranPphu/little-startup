5e4b55fd249a1da53315d32c86e50c5b0bee52ec
phuccuhp211 <phuccuhp211@gmail.com>
2025-05-08 08:40:07 +0000

    change folder

--- backend/src/main/java/com/main/backend/configurations/SecurityConfig.java
+++ backend/src/main/java/com/main/backend/configurations/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.main.backend.authentication.configurations;
+package com.main.backend.configurations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
