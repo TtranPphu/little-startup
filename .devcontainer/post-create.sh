deb45f4cd58cba49951cf81de6cfed1ecce31956
TtranPphu <ttranpphu0212@gmail.com>
2025-05-10 21:54:55 +0700

    Remove nvim .local mount, mount the whole .config instead of only nvim, clone nvim default config

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,1 +1,8 @@
 #! /usr/bin/bash
+
+# default nvim config if not exist
+git-user=TtranPphu
+git-branch=TtranPphu-patch-1
+git clone https://github.com/$git-user/kickstart.nvim \
+  --branch $git-branch "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim \
+  --depth 1 &>/dev/null | true
