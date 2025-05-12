130836ed3251de47b0a08c74b4756489082e6efa
phuccuhp211 <phuccuhp211@gmail.com>
2025-05-12 16:46:38 +0700

    #26 - fix: Switch from JavaMailSender to MailSender because JavaMailSender requires creating a new Bean instance taht doesnt use the configs in the application.yml

--- backend/src/main/java/com/main/backend/MainBackendApplication.java
+++ backend/src/main/java/com/main/backend/MainBackendApplication.java
@@ -1,7 +1,5 @@
 package com.main.backend;
 
-import java.util.Properties;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
